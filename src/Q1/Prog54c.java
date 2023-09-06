c22a1c0eaccdc8afe6ccf38db92400b5afa1d96a
SanAndreasDOT <schoonover.jjanesville.k12.wi.usj@gmail.com>
2023-09-06 11:36:22 -0500

    Update of 9/6

---
+++ src/Q1/Prog54c.java
@@ -0,0 +1,35 @@
+package Q1;
+
+import java.util.Scanner;
+
+public class Prog54c {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter Radius: ");
+        int rad = input.nextInt();
+        System.out.println();
+
+        System.out.print("Please enter circumference: ");
+        int cir = input.nextInt();
+
+        double area = (int) (3.14159 * rad ^ 2);
+        System.out.println("Area = "+area);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}

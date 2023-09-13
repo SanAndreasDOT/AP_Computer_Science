bbe72a0e38f30a519b35eda2b135210bf181db95
SanAndreasDOT <schoonover.jjanesville.k12.wi.usj@gmail.com>
2023-09-13 11:26:10 -0500

    Update of 9/13

--- src/Q1/Prog76a.java
+++ src/Q1/Prog76a.java
@@ -1,13 +1,22 @@
 package Q1;
-
+import java.util.*;
 public class Prog76a {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number you dislike between 1 - 9 ");
+
+        int dislike = input.nextInt();
+
+        int sum = dislike * 9;
+
+        int boom = sum * 12345679;
 
 
 
-
-
+        System.out.println(dislike + " * 9  = " + sum);
+        System.out.println (dislike + " and " + sum + " * 12345679 ");
+        System.out.println (" = " + boom + " Haha Sucker");
 
 
 

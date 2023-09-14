c72b035b51adbf0d6eccdb0c8c3fe1df62abc7bd
SanAndreasDOT <schoonover.jjanesville.k12.wi.usj@gmail.com>
2023-09-14 11:10:17 -0500

    Update of (ERROR INCOMPLETE FILES)

--- src/Q1/Prog93a.java
+++ src/Q1/Prog93a.java
@@ -1,9 +1,16 @@
 package Q1;
-
+import java.util.*;
 public class Prog93a {
     public static void main(String[] args) {
+        double base = 4.75;
+        double surcharge = 0.1;
+        double cityutil = 0.03;
+        double latefee = 0.04;
+        double total = 0;
 
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Energy Used ");
+        double UserInput = input.nextDouble();
 
 
 

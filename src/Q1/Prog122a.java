8c0415cce1d54352bf0ddeb807c61a36a632bf6e
SanAndreasDOT <schoonover.jjanesville.k12.wi.usj@gmail.com>
2023-09-20 15:33:59 -0500

    Update of (INCOMPLETE FILES)

--- src/Q1/Prog122a.java
+++ src/Q1/Prog122a.java
@@ -5,7 +5,8 @@ public class Prog122a {
 
         int lcv = 0;
         int lcvsq = 0;
-        int lcvsqr = 0;
+        double lcvsqr = 0;
+
 
         while (lcv < 50) {
             lcv += 1; // lvc = lcv + 1;
@@ -13,9 +14,13 @@ public class Prog122a {
         }
 
         while (lcvsq < 2500) {
-            lcvsq += Math.pow(lcvsq, 2);
+            lcvsq += (int) Math.pow(lcvsq, lcv);
+            System.out.println(lcvsq);
+        }
 
-            System.out.println(                      lcvsq);
+        while (lcvsqr < 7.0711); {
+            lcvsqr += Math.sqrt(lcvsq);
+            System.out.println(lcvsqr);
         }
     }
 }
---
+++ src/Q1/Prog122b.java
@@ -0,0 +1,29 @@
+package Q1;
+import java.util.*;
+public class Prog122b {
+    public static void main(String[] args) {
+
+        System.out.print("Please enter hour(s) worked: ");
+        Scanner input = new Scanner(System.in);
+        int Hr = input.nextInt();
+        System.out.println();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}

0d910a3b3e54d166dc02031ea330af93bdb4980e
SanAndreasDOT <schoonover.jjanesville.k12.wi.usj@gmail.com>
2023-09-27 11:35:22 -0500

    Update of 9/27

--- src/Q1/Prog65h.java
+++ src/Q1/Prog65h.java
@@ -1,8 +1,17 @@
 package Q1;
-
+import java.util.*;
 public class Prog65h {
     public static void main(String[] args) {
-    //Jump Start?
+
+
+
+
+        int pounds = 0;
+        int shillings = 0;
+        int pence = 0;
+        double Decimal_pounds = 0.00;
+
+
 
 
 

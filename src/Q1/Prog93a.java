c518af4fcb3d2b803be9c119114f159b6915209d
SanAndreasDOT <schoonover.jjanesville.k12.wi.usj@gmail.com>
2023-10-04 11:36:01 -0500

    Update of 10/4

--- src/Q1/Prog93a.java
+++ src/Q1/Prog93a.java
@@ -7,21 +7,36 @@ public class Prog93a {
 
         double base = 0.0475;
         double surcharge = 0.1;
-        double cityutil = 0.3;
+        double basetax = 0;
+        double cityutil = 0.03;
+        double citytaxtot = 0;
         double latefee = 0.4;
+        double latefeetot = 0;
         double total = 0;
 
         Scanner input = new Scanner(System.in);
         System.out.print("Enter Energy Used ");
-        double UserInput = input.nextDouble();
+        int UserInput = input.nextInt();
+
+        UserInput = (int) (UserInput * base);
+        basetax = UserInput + (UserInput * surcharge);
+        citytaxtot = UserInput + (UserInput * cityutil);
+        latefeetot = UserInput + (UserInput * latefee);
+        total = basetax + citytaxtot + latefeetot;
 
 
-            total = base  * UserInput + surcharge + cityutil;
+
+
+
+
             System.out.println(UserInput);
             System.out.println(base);
             System.out.println(surcharge);
+            System.out.println(basetax);
             System.out.println(cityutil);
+            System.out.println(citytaxtot);
             System.out.println(latefee);
+            System.out.println(latefeetot);
             System.out.println(total);
 
 

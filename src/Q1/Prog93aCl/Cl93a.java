0f6e23305b5029c75eb5bcaf25f03594513f0948
SanAndreasDOT <schoonover.jjanesville.k12.wi.usj@gmail.com>
2023-10-13 11:22:47 -0500

    Update of 10/13

--- src/Q1/Prog93aCl/Cl93a.java
+++ src/Q1/Prog93aCl/Cl93a.java
@@ -1,14 +1,15 @@
 package Q1.Prog93aCl;
 
-public class Cl93a {
+public class Cl93a { //Rectangle (Helper Class)
     private int kwatts;
     private double surcharge;
     private double baserate;
     private double citytax;
     private double total;
     private double latetotal;
-
+    // Input Section (Constructor)
     public Cl93a (int kw) {
+        // Set up Private data
         kwatts = kw;
         surcharge = 0.03;
         baserate = 0.0475;
@@ -16,8 +17,11 @@ public class Cl93a {
         total = 0;
         latetotal = 0.04;
     }
+
+    // Calculation Section (Mutators/Setters)
     public void calc(){
-        total = kwatts * 0.0475;
+        total = Math.round(kwatts * 0.0475);
+
 
 
     }
--- src/Q1/Prog93aCl/Prog93a.java
+++ src/Q1/Prog93aCl/Prog93a.java
@@ -1,7 +1,19 @@
 package Q1.Prog93aCl;
-
+import java.util.*;
 public class Prog93a {
     public static void main(String[] args) {
+        // Input Section
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Enter Energy Used: ");
+        int kwatts = input.nextInt();
+        // Calculation Section
+        Cl93a data = new Cl93a(kwatts);
+        data.calc();
+
+        String output = data.toString();
+
+        //Output Section
+        System.out.println(output);
     }
 }

fbc630c60accb57caca2d137d25ba7e51f76b56f
SanAndreasDOT <schoonover.jjanesville.k12.wi.usj@gmail.com>
2023-10-19 11:21:43 -0500

    Update of 10/19

--- src/Q1/Prog93aCl/Cl93a.java
+++ src/Q1/Prog93aCl/Cl93a.java
@@ -1,26 +1,25 @@
 package Q1.Prog93aCl;
 
+import static java.lang.Math.round;
+
 public class Cl93a { //Rectangle (Helper Class)
-    private int kwatts;
-    private double surcharge;
-    private double baserate;
-    private double citytax;
+    private final int kwatts;
     private double total;
-    private double latetotal;
+
     // Input Section (Constructor)
     public Cl93a (int kw) {
         // Set up Private data
         kwatts = kw;
-        surcharge = 0.03;
-        baserate = 0.0475;
-        citytax = 0.1;
+        double surcharge = 0.03;
+        double baserate = 0.0475;
+        double citytax = 0.1;
+        double latetotal = 0.04;
         total = 0;
-        latetotal = 0.04;
     }
 
     // Calculation Section (Mutators/Setters)
     public void calc(){
-        total = Math.round(kwatts * 0.0475);
+        total = round(kwatts * 0.0475);
 
 
 

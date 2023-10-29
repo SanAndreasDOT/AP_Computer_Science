acc003d32bd9dc1ea3f4024f90cfa3df8415ed13
SanAndreasDOT <schoonover.jjanesville.k12.wi.usj@gmail.com>
2023-10-29 16:07:18 -0500

    Update of (INCOMPLETE FILES)

--- src/Q1/Prog58b.java
+++ src/Q1/Prog58b.java
@@ -1,4 +1,33 @@
 package Q1;
-
+import java.util.*;
 public class Prog58b {
+    public static void main(String[] args) {
+
+     Scanner input = new Scanner(System.in);
+     System.out.println("Enter First Number");
+     int a = input.nextInt();
+     System.out.println("Enter Second Number");
+     int b = input.nextInt();
+     System.out.println("Enter Third Number");
+     int c = input.nextInt();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //test output
+        // System.out.println(a + " " + b + " "+ c);
+
+    }
 }
+
+   

e5a79ff83c1f83b88203a55061d2e5d5f348cafd
SanAndreasDOT <schoonover.jjanesville.k12.wi.usj@gmail.com>
2023-10-31 11:24:13 -0500

    Update of 10/31

--- src/Q1/Prog58t.java
+++ src/Q1/Prog58t.java
@@ -1,7 +1,45 @@
 package Q1;
 
+import java.util.Scanner;
+
 public class Prog58t {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Cost Of Item");
+        double cost = input.nextDouble();
+
+        System.out.println("Enter Amount Payed");
+        double payed = input.nextDouble();
+
+
+        int dollars = 0;
+        int q = 0;
+        int dimes = 0;
+        int n =0;
+        int p = 0;
+        double amtDue = payed - cost;
+
+        dollars = (int) (amtDue / 1.00);
+
+
+
+
+
+
+
+
+
+
+
+
+        System.out.printf("Change is $%.2f\n", amtDue);
+        System.out.println("Dollars: " + dollars);
+        System.out.println("Quarters:  " + q);
+        System.out.println("Dimes: " + dimes);
+        System.out.println("Nickles: " + n);
+        System.out.println("Pennys: " + p);
+
+
 
     }
 }

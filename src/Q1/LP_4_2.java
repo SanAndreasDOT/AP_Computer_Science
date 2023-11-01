a70cbb2101b86758cce0d8e31e2f06abc17622d1
SanAndreasDOT <schoonover.jjanesville.k12.wi.usj@gmail.com>
2023-11-01 11:34:38 -0500

    Update of 11/1

--- src/Q1/LP_4_2.java
+++ src/Q1/LP_4_2.java
@@ -1,12 +1,39 @@
 package Q1;
 
+import java.util.Scanner;
+
 public class LP_4_2 {
     public static void main(String[] args) {
 
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Length: ");
+        int Length = input.nextInt();
+        System.out.print("Enter Width: ");
+        int Width = input.nextInt();
+        System.out.print("Enter Height: ");
+        int Height = input.nextInt();
+        System.out.print("Enter Weight: ");
+        int Weight = input.nextInt();
+        int Size = 0;
 
+        Size = Length * Weight * Height;
 
+        if (Weight > 27 & Size > 100000) {
+            System.out.println("Too Heavy and to big");
 
+        }
+         if (Weight > 27) {
+             System.out.println("To Heavy");
+         }
+
+         if (Size > 100000) {
+             System.out.println("Too Big");
+         }
+
+         else{
+             System.out.println("Size is: " + Size);
+         }
 
 
     }

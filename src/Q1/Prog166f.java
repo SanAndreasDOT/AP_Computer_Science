d72108d5208474ffa3ccedd912cd07f3a2841680
SanAndreasDOT <schoonover.jjanesville.k12.wi.usj@gmail.com>
2023-11-08 11:36:46 -0600

    Update of 11/8

--- src/Q1/Prog166f.java
+++ src/Q1/Prog166f.java
@@ -9,9 +9,6 @@ public class Prog166f {
             int sum = 0;
             for (int i = 0; i <= 10; i++) {
                 System.out.println(i);
-                int num1 = 0;
-
-
             }
             System.out.println(sum);
         }

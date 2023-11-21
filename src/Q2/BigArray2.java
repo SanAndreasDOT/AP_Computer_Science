9574c6edd63b7dc636f64e7234e2b2c5a08dd1ec
SanAndreasDOT <schoonover.jjanesville.k12.wi.usj@gmail.com>
2023-11-21 11:03:41 -0600

    Update of 11/21

--- src/Q2/BigArray2.java
+++ src/Q2/BigArray2.java
@@ -29,17 +29,17 @@ public class BigArray2 {
                cats[lcv] = new Cat(name, weight, age, cost);
             }
 
-            System.out.println("2. All the cats: ");
+            System.out.println("1. All the cats: ");
             System.out.println("Name\tWeight\tAge\tCost");
             for (int lcv = 0; lcv < numCats; lcv++) {
                 printCat(cats[lcv]);
             }
             // #2
-            System.out.println("\t2. The Third cat is named: " + cats[2].getName());
+            System.out.println("2. The Third cat is named: " + cats[2].getName());
 
             // #3
             cats[numCats-1].setWeight(cats[numCats-1].getWeight() + 10);
-            System.out.println("3. the updated weight is: " + cats[numCats-1].getWeight());
+            System.out.println("3. The updated weight is: " + cats[numCats-1].getWeight());
 
             //#4
             int rascalIndex = 0;
@@ -80,13 +80,38 @@ public class BigArray2 {
             cats[3] = tempCat;
 
             //#10
+            System.out.println("\n10. The current cat names are: ");
 
+            for (Cat cat : cats) {
+                if (cat != null) System.out.print(cat.getName() + "\t");
+            }
+            System.out.println();
 
+            //11
+            int i = 0;
+            while (i < numCats) {
+                if (cats[i].getCost() < 26){
+                    for (int j = i; j<numCats-1; j++)
+                        cats[j] = cats[j+1];
+                    numCats--;
+                }else {
+                    i++;
+                }
+            }
 
+            System.out.println("\n11. The cats costing $26 or more actually cost:");
+            for (int lcv=0; lcv<numCats;lcv++)
+                System.out.print(cats[lcv].getCost() + " ");
+            System.out.println();
 
-
-
-
+            //12
+            System.out.println("\n12. The cats being put on a diet are: ");
+            for (int lcv = 0; lcv < numCats; lcv++) {
+                Cat cat = cats[lcv];
+                if (cat.getWeight() > 15)
+                    System.out.print(cat.getName() + "\t");
+            }
+            System.out.println();
 
 
         } catch (IOException e) {
@@ -94,3 +119,37 @@ public class BigArray2 {
         }
     }
 }
+/*
+1. All the cats:
+Name	Weight	Age	Cost
+Inky	15.69	2	35.79
+Panda	14.3	6	15.03
+Rascal	21.1	21	0.0
+Blacky	13.99	3	26.89
+Taffy	24.5	10	56.89
+Toby	17.2	10	37.57
+2. The Third cat is named: Rascal
+3. The updated weight is: 27.2
+
+7. The updated list is:
+Name	Weight	Age	Cost
+Inky	15.69	2	35.79
+Angel	3.6	1	25.99
+Panda	14.3	6	15.03
+Blacky	13.99	3	26.89
+Taffy	24.5	10	56.89
+Toby	27.2	10	37.57
+Gimpy	14.3	10	29.99
+
+10. The current cat names are:
+Inky	Blacky	Suger	Angel	Taffy	Toby	Gimpy	Panda
+
+11. The cats costing $26 or more actually cost:
+35.79 26.89 33.25 56.89 37.57 29.99
+
+12. The cats being put on a diet are:
+Inky	Suger	Taffy	Toby
+
+Process finished with exit code 0
+
+ */

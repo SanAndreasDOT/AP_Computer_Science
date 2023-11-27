0b7b87144854cc99d622db77b8fce3a3b2f0491d
SanAndreasDOT <schoonover.jjanesville.k12.wi.usj@gmail.com>
2023-11-27 11:23:34 -0600

    Update of 11/27

---
+++ src/Q2/LP4_8/LP4_8.java
@@ -0,0 +1,23 @@
+package Q2.LP4_8;
+
+import Q1.Prog88aCl.Cl88a;
+
+import java.util.Scanner;
+
+public class LP4_8 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter A: ");
+        int a = input.nextInt();
+        System.out.println("Enter C: ");
+        int c = input.nextInt();
+        System.out.println("Enter M: ");
+        int m = input.nextInt();
+        System.out.println("Enter Seed: ");
+        int x = input.nextInt();
+
+        for (int lcv = 0; lcv < 10; lcv++) {
+
+        }
+    }
+}

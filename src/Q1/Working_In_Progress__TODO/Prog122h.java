e5a81f9c948a24840dde98e453ed130215f6f80e
SanAndreasDOT <schoonover.jjanesville.k12.wi.usj@gmail.com>
2023-11-28 11:12:38 -0600

    Update of 11/28

--- src/Q1/Working_In_Progress__TODO/Prog122h.java
+++ src/Q1/Working_In_Progress__TODO/Prog122h.java
@@ -2,6 +2,21 @@ package Q1.Working_In_Progress__TODO;
 import java.util.*;
 public class Prog122h {
     public static void main(String[] args) {
+        int num = 0;
+        int sqr = 0;
+        double sqaroot = 0;
+        int cube = 0;
+        double fourroot = 0;
+
+
+
+
+        for (int lcv = 0; lcv < 20; lcv++) {
+            num += 1;
+
+
+            System.out.println(num);
+        }
 
     }
 }

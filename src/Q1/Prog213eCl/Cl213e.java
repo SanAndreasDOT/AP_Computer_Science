d536d6517faa813f381f5679cc53aa28a6695d0a
SanAndreasDOT <schoonover.jjanesville.k12.wi.usj@gmail.com>
2023-11-30 12:28:39 -0600

    Update of 11/30

--- src/Q1/Prog213eCl/Cl213e.java
+++ src/Q1/Prog213eCl/Cl213e.java
@@ -1,4 +1,29 @@
 package Q1.Prog213eCl;
 
 public class Cl213e {
+private int age;
+private int group;
+private double percent;
+
+
+public Cl213e (int old, int people, double pcent) {
+    age = old;
+    group = people;
+    percent = pcent;
+}
+
+
+public void calc() {
+
+
+
+
+
+
+
+
+}
+
+public String toString() { return }
+
 }
--- src/Q1/Prog213eCl/Prog213e.java
+++ src/Q1/Prog213eCl/Prog213e.java
@@ -1,4 +1,26 @@
 package Q1.Prog213eCl;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Prog213e {
+    public static void main(String[] args) {
+        try {
+            Scanner input = new Scanner (new File("Langdat/prog213e.dat"));
+            while (input.hasNext()) {
+                int age = input.nextInt();
+                int pple = input.nextInt();
+                double percent = input.nextDouble();
+
+                Cl213e calc = new Cl213e(age, pple, percent);
+                calc.calc();
+
+            }
+
+        } catch (IOException e) {
+            System.out.println("can't find data file!");
+        }
+
+    }
 }

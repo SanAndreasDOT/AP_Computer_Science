d14c4879474ff08bd657dab46bd6ed5127d1cea9
SanAndreasDOT <schoonover.jjanesville.k12.wi.usj@gmail.com>
2023-12-01 11:24:47 -0600

    Update of 12/01

--- src/Q2/ArrayListLab/MyArrayListLabClass.java
+++ src/Q2/ArrayListLab/MyArrayListLabClass.java
@@ -1,4 +1,16 @@
 package Q2.ArrayListLab;
 
 public class MyArrayListLabClass {
+    private int[] myArray;
+    private int mySize;
+    private int myCapacity;
+    private final int DEFAULT_SIZE = 16;
+
+
+    public IntArrayList() {
+        myArray = new int[DEFAULT_SIZE];
+        mySize = 0;
+        myCapacity = DEFAULT_SIZE;
+    }
+
 }
